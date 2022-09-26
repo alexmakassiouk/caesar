ae8d4db903b2f607fee6e7cccfa9ae40a39e3179
alexmak <alexmak@stud.ntnu.no>
2022-09-26 16:47:57 +0200

    Added decryption by spaces and tested for bigger samples

--- caesar.py
+++ caesar.py
@@ -3,30 +3,25 @@ from alphabets import ALPHABETS, ENGLISH_ALPHABET, NORWEGIAN_ALPHABET, GREEK_ALP
 from commonWords import common_words
 import re
 
-
+### FILE HANDLING ###
 def load_text(filename):
     with open('data/' + filename) as f:
         text = f.read()
         return text
-
 def write_file(filename, data):
     with open('data/' + filename, 'w') as f:
         f.write(data)
+###-----------------###
 
+### CORE METHODS ###
 def offset_alphabet(alphabet, offset):
     #If offset is greater than length of alphabet: Find the offset modulus the length of alphabet
     offset = offset%len(alphabet)
-    new_alphabet = alphabet[offset:] + alphabet[:offset]
-    return new_alphabet
-
-def offset_another_alphabet(original_alphabet, new_alphabet, offset):
-    #TODO
-    #Fungerer vel kanskje ikke å få en én-til-én mapping mellom alfabeter av ulik lengde
-    pass
-
+    shifted_alphabet = alphabet[offset:] + alphabet[:offset] #Permutates alphabet. Starts with character on index 'offset' in original alphabet and ends with character on index 'offset'-1
+    return shifted_alphabet
 
 def encrypt(text, alphabet, offset):
-    special_chars = ['\n', ',', '.', '-', '_', '"', '\t', '/', '!', '?', '$', ':', ';', "'", '#', '%', '&', '(', ')', '=', '@', '*']
+    special_chars = ['\n', ',', '.', '-', '_', '"', '\t', '/', '!', '?', '$', ':', ';', "'", '#', '%', '&', '(', ')', '=', '@', '*', '–']
     encryption_alphabet = offset_alphabet(alphabet, offset)
     encrypted_text = ""
     for char in text:
@@ -42,11 +37,12 @@ def encrypt(text, alphabet, offset):
 
 def decrypt(text, alphabet, offset):
     return encrypt(text, alphabet, -offset)
+###----------------###
 
 
+### ADDITIONAL FUN ###
 def decrypt_without_alphabet(text):
-    alphabet_index = alphabet_recognition(text)
-    alphabet = ALPHABETS[alphabet_index]
+    alphabet_index = alphabet_recognition(text) #Runtime may be much better, but no practical implications on decently sized texts
     match alphabet_index:
         case 0:
             return decrypt_without_offset(text, ENGLISH_ALPHABET)
@@ -55,21 +51,31 @@ def decrypt_without_alphabet(text):
         case _:
             return "No support for this alphabet yet!"
 
-def decrypt_without_offset(text, alphabet):
+def decrypt_without_offset(text, alphabet): # Finds the offset for which shifted_text yields most amount of words in common_words
     matches = [0] * len(alphabet)
     for i in range(1,len(alphabet)):
         shifted_text = encrypt(text, alphabet, i)
         encrypted_words = re.split(', |\ |_|-|!', shifted_text)
         for word in common_words[alphabet_recognition(alphabet)]:
-            if word in encrypted_words:
+            if word in encrypted_words: #Biggest concern on runtime
                 matches[i]+=1
-    best_offset = indexOf(matches, max(matches))
+    if max(matches) != 0:
+        best_offset = indexOf(matches, max(matches))
+        return encrypt(text, alphabet, best_offset)
+    else:
+        return decrypt_by_spaces(text, alphabet)
+
+def decrypt_by_spaces(text, alphabet): # Finds the offset which yields most amount of spaces in shifted_text
+    spaces = [0] * len(alphabet)
+    for i in range(1, len(alphabet)):
+        shifted_text = encrypt(text, alphabet, i)
+        for char in shifted_text:
+            if char == " ":
+                spaces[i]+=1
+    best_offset = indexOf(spaces, max(spaces))
     return encrypt(text, alphabet, best_offset)
 
-
-# Ideer for dekryptering uten å vite offset: 
-# Se på antall mellomrom. Hvor mange gjennomsnittlige mellomrom ifht. antall bokstaver?
-# Bruke de mest brukte ordene i ulike språk
+# Enda flere ideer for å dekryptere: Telle forekomster av punktum etterfulgt av mellomrom og stor bokstav.
 
 def alphabet_recognition(text): 
     english_counter = norwegian_counter = greek_counter = cyrillic_counter = hebrew_counter = 0
@@ -81,11 +87,17 @@ def alphabet_recognition(text):
                 counters[i]+=1
             i+=1
     return indexOf(counters, max(counters))
+###-----------------###
 
-kryptertNorsk = load_text("kryptertFil.txt")
-print(decrypt_without_alphabet(kryptertNorsk))
+
 
 ###### TESTING ######
+
+
+kryptertNorsk = load_text("encryptedVGArticle.txt")
+print(decrypt_by_spaces(kryptertNorsk, NORWEGIAN_ALPHABET))
+
+
 #encryptedEnglish = load_text("encryptedEnglish.txt")
 #print(decrypt_without_alphabet(encryptedEnglish))
 
@@ -100,6 +112,7 @@ print(decrypt_without_alphabet(kryptertNorsk))
 #print(decrypt(encrypted_text, CYRILLIC_ALPHABET, 17))
 #print(alphabet_recognition(encrypted_text))
 
+### MAIN ###
 #def main():
 #    filename = input("Skriv inn filnavn: (.txt)")
 #    encrypt_char = input("Kryptere eller dekryptere? (K/D)")
@@ -116,4 +129,6 @@ print(decrypt_without_alphabet(kryptertNorsk))
 #        filename = input("filnavn: ") + ".txt"
 #    write_file(filename, new_text)
 #    print("Lagret til ", filename, "!")
+###-------------###
+
 #main()
